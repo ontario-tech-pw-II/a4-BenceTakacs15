f85c02a268c1f54aa5e864597dbc32c4a0eb287b
Bence Takacs <bence@LAPTOP-V9AL5I95.localdomain>
2020-02-13 12:14:22 -0500

    Completed

--- Circle.cpp
+++ Circle.cpp
@@ -1,100 +1,104 @@
+#include <cmath>
 #include <iostream>
 using namespace std;
 
-class Circle {
-  private:  
-    int x,y;      // center of circle
-    double r;    // radius of circle
+class Circle
+{
+private:
+	int x, y; // center of circle
+	double r; // radius of circle
 
-  public:
-   
-    Circle(int =0,int=0, double =1.0);
-    void print() const;
-    double area() const;
+public:
+	Circle(int = 0, int = 0, double = 1.0);
+	void print() const;
+	double area() const;
 
-    double operator+(const Circle &) const;
+	double operator+(const Circle&) const;
 
-    Circle & operator++();
-    Circle operator++(int);
+	Circle& operator++();
+	Circle operator++(int);
 
-  friend ostream & operator<<(ostream &out, const Circle &);
-  friend istream & operator>>(istream &in, Circle &);
+	friend ostream& operator<<(ostream& out, const Circle&);
+	friend istream& operator>>(istream& in, Circle&);
 };
 
-Circle::Circle(int x,int y, double r)
+Circle::Circle(int x, int y, double r)
 {
-  // Initialize the object here
+	// Initialize the object here
+	this->x = x;
+	this->y = y;
+	this->r = r;
 }
 
-double Circle::area() const {
-  // Return the area of a circle  
+double Circle::area() const
+{
+	// Return the area of a circle
+	return M_PI * r * r;
 }
 
-double Circle::operator+(const Circle & c) const
+double Circle::operator+(const Circle& c) const
 {
-   // + Return the area of two circles  
-
+	// + Return the area of two circles
+	return area() + c.area();
 }
 
-Circle & Circle::operator++()
+Circle& Circle::operator++()
 {
-  // Increment the radius by 1
+	// Increment the radius by 1
+	r++;
 
-  return *this;
+	return *this;
 }
 
 Circle Circle::operator++(int)
 {
-  Circle temp = *this;
-  
-  //increment the radius by 1
+	Circle temp = *this;
 
-  return temp;
+	// increment the radius by 1
+	r++;
+
+	return temp;
 }
 
-ostream & operator<<(ostream &out, const Circle &c)
+ostream& operator<<(ostream& out, const Circle& c)
 {
-
-  // print a circle in the out stream (the format is the same as print())
-  return out;
+	// print a circle in the out stream (the format is the same as print())
+	return out << "Circle =(" << c.x << "," << c.y << "," << c.r << ")";
 }
 
-istream & operator>>(istream &in, Circle &c)
+istream& operator>>(istream& in, Circle& c)
 {
- 
- // Read a circle information form the in stream
-
-  return in;
+	// Read a circle information form the in stream
+	in >> c.x;
+	in >> c.y;
+	in >> c.r;
+	return in;
 }
-
 
 void Circle::print() const
 {
-  cout << "Circle =(" << x << "," << y << "," << r << ")" << endl;
+	cout << "Circle =(" << x << "," << y << "," << r << ")" << endl;
 }
-
-
 
 int main()
 {
-  // Circle a;
-  // Circle b(2,3,3.0);
+	Circle a;
+	Circle b(2,3,3.0);
 
-  // a.print();
-  // b.print();
-  
-  // cout << "The area a + b is: " << a+b << endl;
-  // ++a;
-  // a.print();
+	a.print();
+	b.print();
 
-  // a++;
-  // a.print();
+	cout << "The area a + b is: " << a+b << endl;
+	++a;
+	a.print();
 
-  // cout << a;
+	a++;
+	a.print();
 
-  // cin >> a;
-  // cout <<a;
+	cout << a;
 
-  return 0;
+	cin >> a;
+	cout <<a;
 
+	return 0;
 }
